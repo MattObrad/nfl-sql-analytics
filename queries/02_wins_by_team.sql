78a45a6397cfee3cab67ee87be2bf8ee8001243f
MattObrad <m.obradovich1@gmail.com>
2025-12-23 10:52:33 -0600

    Create 02_wins_by_team.sql

---
+++ queries/02_wins_by_team.sql
@@ -0,0 +1,13 @@
+# Question:
+## Which teams have the most wins across all seasons?
+
+USE nfl_analytics;
+
+SELECT
+  winner AS team,
+  COUNT(*) AS total_wins
+FROM games_cleaned
+WHERE winner IS NOT NULL
+  AND winner <> 'TIE'
+GROUP BY winner
+ORDER BY total_wins DESC;
