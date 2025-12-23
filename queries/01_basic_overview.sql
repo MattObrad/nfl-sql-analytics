c436e38904b226cf425cea7fc38fa8224d4a12c1
MattObrad <m.obradovich1@gmail.com>
2025-12-23 10:50:00 -0600

    Create 01_basic_overview.sql

---
+++ queries/01_basic_overview.sql
@@ -0,0 +1,10 @@
+# Question:
+## How many NFL games are in the dataset, and what seasons does it cover?
+
+USE nfl_analytics;
+
+SELECT
+  COUNT(*) AS total_games,
+  MIN(season) AS first_season,
+  MAX(season) AS last_season
+FROM games_cleaned;
